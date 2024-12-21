abfeafe4ce3ef31d76ddf6bbd4dc1c540cf9e7cf
seohyun-lee <applemint2003@gmail.com>
2024-12-22 08:00:34 +0900

    feat: api 개발중

--- src/domain/song/services.py
+++ src/domain/song/services.py
@@ -1,17 +1,24 @@
-from src.domain.song.crud import insert, get_many
-from datetime import datetime
+from core.crud import get_many, get_by_id
 from typing import List
 
-# 노래 생성
-async def create_song(song_data: dict) -> str:
-    song_data["createdAt"] = datetime.now()
-    song_data["modifiedAt"] = datetime.now()
-    return await insert("songs", song_data)
 
 # 카테고리별 노래 목록 가져오기
 async def get_songs_by_category(category: str, skip: int, limit: int) -> List[dict]:
     return await get_many("songs", {"category": category}, skip, limit)
 
+# obj_id로 노래 가져오기
+async def get_song_by_obj_id(obj_id: object):
+    return await get_by_id("songs", obj_id)
+    
+
+'''
+# 노래 생성
+async def create_song(song_data: dict) -> str:
+    song_data["createdAt"] = datetime.now()
+    song_data["modifiedAt"] = datetime.now()
+    return await insert("songs", song_data)
+    
 # 노래 검색 (페이징)
 async def search_songs(query: str, skip: int, limit: int) -> List[dict]:
     return await get_many("songs", {"title": {"$regex": query, "$options": "i"}}, skip, limit)
+'''
--- src/domain/wish/services.py
+++ src/domain/wish/services.py
@@ -1,15 +1,15 @@
-from src.domain.wish.crud import insert, get_by_id, get_many
+from core.crud import insert, get_by_id, get_many
 from datetime import datetime
 from typing import List
 
 # 소원 생성
-async def create_wish(nickname: str, content: str, songid: str) -> str:
+async def create_wish(nickname: str, content: str, song_id: str) -> str:
     wish_data = {
         "nickname": nickname,
         "content": content,
-        "isDisplayed": True,
-        "createdAt": datetime.now(),
-        "songid": songid,
+        "is_displayed": True,
+        "created_at": datetime.now(),
+        "song_id": song_id,
     }
     return await insert("wishes", wish_data)
 
@@ -22,3 +22,9 @@ async def get_random_wishes(limit: int = 4) -> List[dict]:
     # MongoDB의 $sample 사용 (별도 crud 함수 필요할 수도 있음)
     wishes = await db["wishes"].aggregate([{"$sample": {"size": limit}}]).to_list(length=limit)
     return wishes
+
+
+# 특정 노래의 wish 갯수 세기
+async def count_wishes_by_song_id(song_id: str) -> int:
+    wishes = await get_many("wishes", {"song_id": song_id}, 0, 0)  # 모든 wish를 가져옴
+    return len(wishes)
